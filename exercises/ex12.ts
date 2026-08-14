efe184fbf930d6cd62783a29038e4e0052ab5566
Sae Nakatani <nakatanisae0218@gmail.com>
2026-08-14 00:13:02 -0700

    Complete ex12

--- exercises/ex12.ts
+++ exercises/ex12.ts
@@ -1,8 +1,12 @@
 // Write a function called areAllCharactersUnique that takes in a string and
 // returns true or false depending on whether all characters in the string are unique (i.e., no character is repeated).
 
-const areAllCharactersUnique = () => {
+const areAllCharactersUnique = (char:string):boolean => {
   // Code here
+  
+  const charSet = new Set(char);
+  return char.length === charSet.size;
+  
 };
 
 export default areAllCharactersUnique;
