a96e5fae2400716ce5ae32d456885198c5b2f19c
Sae Nakatani <nakatanisae0218@gmail.com>
2026-08-13 23:52:08 -0700

    Complete ex11

--- exercises/ex11.ts
+++ exercises/ex11.ts
@@ -8,8 +8,26 @@
 // If three people like it, it should return '{name1}, {name2} and {name3} like this'
 // If more than three people like it, it should return '{name1}, {name2} and {x} others like this'
 
-const displayLikes = () => {
+const displayLikes = (names: string[]):string => {
   // Code here
+  switch (names.length) {
+    case 0:
+      return 'no one likes this';
+      break;
+    case 1:
+      return `${names[0]} likes this`;
+      break;
+    case 2:
+      return `${names[0]} and ${names[1]} like this`;
+      break;
+    case 3:
+      return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+      break;
+    default:
+      return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
+      break;
+  }
+
 };
 
 export default displayLikes;
